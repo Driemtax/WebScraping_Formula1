1d28508f251e3c712004d3392dd611ad1fc501db
Driemtax <driemtax@gmail.com>
2024-07-14 18:12:11 +0200

    Basic table structure for frontend

---
+++ frontend/f1-frontend/src/TeamStandings/Standings.tsx
@@ -0,0 +1,22 @@
+function Standings(){
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <td>Pos</td>
+                    <td>Team</td>
+                    <td>Points</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1.</td>
+                    <td>Mercedes</td>
+                    <td>192</td>
+                </tr>
+            </tbody>
+        </table>
+    );
+}
+
+export default Standings;
